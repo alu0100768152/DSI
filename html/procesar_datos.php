83643195d8f2d9a6941200886437c3b6113086f7
Cristian Ángel Díaz <alu0100891843@ull.edu.es>
2018-04-22 16:28:55 +0100

    BBDD de datos de usuario. Creación procesar_datos.php enlace BBDD

---
+++ html/procesar_datos.php
@@ -0,0 +1,23 @@
+<?php
+    $NOMBRE=$_POST['nombre'];
+    $APELLIDOS=$_POST['apellidos'];
+    $USUARIO=$_POST['nombre-usuario'];
+    $CORREO=$_POST['email'];
+    $CONTRASEÑA=$_POST['password'];
+
+    if( isset ($_POST['nombre']) && !empty ($_POST['nombre']) &&
+        isset ($_POST['apellidos']) && !empty ($_POST['apellidos']) &&
+        isset ($_POST['nombre-usuario']) && !empty ($_POST['nombre-usuario']) &&
+        isset ($_POST['email']) && !empty ($_POST['email']) &&
+        isset ($_POST['password']) && !empty ($_POST['password']))
+    {
+        $ENLACE_BD = mysqli_connect("localhost","root","","dsi") or die("ERROR: No se conectó a la BD");
+        // mysql_select_db(dsi,$ENLACE_BD) or die ("ERROR: No se pudo acceder a la BD");
+
+        mysqli_query($ENLACE_BD,"INSERT INTO registro_usuario VALUES('$NOMBRE','$APELLIDOS','$USUARIO','$CORREO','$CONTRASEÑA')");
+        echo "Datos Enviados Correctamente";
+    }
+    else{
+        echo "Error al enviar los datos";
+    }
+?>
