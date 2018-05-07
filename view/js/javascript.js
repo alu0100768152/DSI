f12ff2a63ede4feb708627c2912614e2987c0e42
Cristian Ángel Díaz <alu0100891843@ull.edu.es>
2018-05-07 10:18:47 +0100

    Corrección de expresión regular

--- view/js/javascript.js
+++ view/js/javascript.js
@@ -14,42 +14,34 @@ function nif(dni) {
      letra='TRWAGMYFPDXBNJZSQVHLCKET';
      letra=letra.substring(numero,numero+1);
     if (letra!=letr.toUpperCase()) {
-        // var erroremail=document.createElement('p');
-        // erroremail.innerHTML='No escribiste tu email';
-        // document.getElementById("capa").appendChild(erroremail);
-        //var TextoLinea= 'Dni erróneo';
-        //document.getElementById("capa").innerHTML = "<br>" + "Hola don pepito";  // Agrego nueva linea antes
-        //return false;
-       //alert('Dni erroneo, la letra del NIF no se corresponde');
        return false;
        
      }else{
-        //document.getElementById("capa").innerHTML = 'meh';  // Agrego nueva linea antes
+        // document.getElementById("capadni").innerHTML = 'meh';  // Agrego nueva linea antes
         return true;
 
      }
   }else{
-        //document.getElementById("capa").innerHTML = "<br>" + "Hola don pepito"; // Agrego nueva linea antes
+        // document.getElementById("capadni").innerHTML = "<br>" + "Hola don pepito"; // Agrego nueva linea antes
         return false;
 
    }
 }
+function Verifica2() {
 
-function AgregarLineaDeTexto() {
-  var TextoLinea=document.getElementById("objeto").value;
-    document.getElementById("añadir_texto").innerHTML += "<br>" + TextoLinea;  // Agrego nueva linea antes
-}
-//  function Verifica_notif(){
-//     var compro_nombre= document.getElementById("textarea1").value;
-//     if(compro_nombre==''){
-//        document.getElementById("capa5").innerHTML = "Inserte novedad";
-//        return false;
-//     }
-//     else{
-//         document.getElementById("capa5").innerHTML = " ";
-//         return true;    
-//     }
-// }
+    document.getElementById("capadni").innerHTML = "<br>" + "Hola don pepito";
+
+    var compro_dni = nif(document.getElementById("dni").value);
+
+    if (compro_dni == true) {
+        document.getElementById("capadni").innerHTML = "";  // Agrego nueva linea antes
+        return true;
+    }
+    else {
+        document.getElementById("capadni").innerHTML = "Inserte DNI correctamente";
+        return false;
+    }
+} 
 
 function Verifica_nom(){
     var compro_nombre= document.getElementById("nombre").value;
@@ -109,50 +101,6 @@ function Verifica_ap(){
         return true;    
     }
 }
-function Verifica2(){
-    
-//     var capa = document.getElementById("capa");
-//     var h3 = document.createElement("h3");
-//     h3.innerHTML = document.getElementById("nombre");
-//   //h1.innerHTML = texto;
-//     capa.appendChild(h3);
-
-    //document.getElementById("capa").innerHTML = "<br>" + "Hola don pepito";
-    
-  
-   var compro_dni=nif(document.getElementById("dni").value);
- 
-   if(compro_dni==true){
-        document.getElementById("capa").innerHTML = "";  // Agrego nueva linea antes
-        return true;    
-   }
-   else{
-        document.getElementById("capa").innerHTML = "Inserte DNI correctamente";
-        return false;    
-   }
-//   var capa = document.getElementById("capa");
-//   var h1 = document.createElement("h1");
-//     h1.innerHTML = document.getElementById("nombre");
-//   //h1.innerHTML = texto;
-//   capa.appendChild(h1);
-   
-} 
-
-
-
-
-// function Verifica_all(){
-    
-//     if(Verifica_nom()==true && Verifica_ap()==true && Verifica_pass()==true && Verifica2()==true){
-//         return true;
-//     }else{
-//       return false;
-//     }
-// }
-// var capa = document.getElementById("capa");
-// var h1 = document.createElement("h1");
-// h1.innerHTML = document.getElementById("texto");
-// capa.appendChild(h1);
 
 $(document).ready(function(){
   $('.parallax').parallax();
@@ -183,10 +131,5 @@ $(document).ready(function(){
   $('#modal1').modal('open');
 
   $('#modal1').modal('close');
-
-
-
-
-
 });
 
