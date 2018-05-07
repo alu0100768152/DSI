5c1a2e061de1f99695dbc545b89014ba4fe70cb1
Cristian Ángel Díaz <alu0100891843@ull.edu.es>
2018-05-07 10:31:46 +0100

    Corrección insertar BBDD DNI

--- procesar_datos.php
+++ procesar_datos.php
@@ -4,19 +4,21 @@
     
     $NOMBRE=$_POST['nombre'];
     $APELLIDOS=$_POST['apellidos'];
+    $DNI=$_POST['dni'];
     $USUARIO=$_POST['nombre_usuario'];
     $CORREO=$_POST['email'];
     $CONTRASEÑA=$_POST['password'];
 
     if( isset ($_POST['nombre']) && !empty ($_POST['nombre']) &&
         isset ($_POST['apellidos']) && !empty ($_POST['apellidos']) &&
+        isset ($_POST['dni']) && !empty ($_POST['dni']) &&
         isset ($_POST['nombre_usuario']) && !empty ($_POST['nombre_usuario']) &&
         isset ($_POST['email']) && !empty ($_POST['email']) &&
         isset ($_POST['password']) && !empty ($_POST['password']))
     {
         // mysql_select_db(dsi,$ENLACE_BD) or die ("ERROR: No se pudo acceder a la BD");
         $db = new Conexion();
-        $query = "INSERT INTO `resgistro_usuario`(`nombre`, `apellido`, `nombre_usuario`, `email`, `password`) VALUES('$NOMBRE','$APELLIDOS','$USUARIO','$CORREO','$CONTRASEÑA')";
+        $query = "INSERT INTO `resgistro_usuario`(`nombre`, `apellido`,`dni`, `nombre_usuario`, `email`, `password`) VALUES('$NOMBRE','$APELLIDOS','$DNI','$USUARIO','$CORREO','$CONTRASEÑA')";
         $db->query($query);
         //echo $query;
         
